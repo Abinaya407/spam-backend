0373d3ee4a3399264d59c0cc825527c646260901
Abinaya <abi0407naya@gmail.com>
2026-06-05 18:48:21 +0530

    updated spam model

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://spam-frontend-one.vercel.app"
     ],
     allow_credentials=False,
     allow_methods=["*"],
